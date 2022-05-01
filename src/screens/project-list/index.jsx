cd0a7b1257f89604c83cf0d9a07a4580819d1746
ZimTang <2351061127@qq.com>
2022-05-01 14:06:40 +0800

    feat: 添加工程列表

---
+++ src/screens/project-list/index.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import List from "./list";
+import SearchPanel from "./search-panel";
+import * as qs from "qs";
+import { cleanObject } from "utils";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+export default function ProjectList() {
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+  const [list, setList] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
+      async (response) => {
+        if (response.ok) {
+          setList(await response.json());
+        }
+      }
+    );
+  }, [param]);
+
+  useEffect(() => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  }, []);
+
+  return (
+    <div>
+      <SearchPanel param={param} setParam={setParam} users={users} />
+      <List list={list} users={users} />
+    </div>
+  );
+}
