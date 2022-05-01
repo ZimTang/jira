44be33fff642bf564f6ed4d92c38a5633f0c33d1
ZimTang <2351061127@qq.com>
2022-05-01 15:39:31 +0800

    chore: 添加ts类型定义

---
+++ src/screens/project-list/index.tsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import List from "./list";
+import SearchPanel from "./search-panel";
+import * as qs from "qs";
+import { cleanObject, useDebounce, useMount } from "utils";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+export default function ProjectList() {
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+  const debouncedParam = useDebounce(param, 200);
+  const [list, setList] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
+    ).then(async (response) => {
+      if (response.ok) {
+        setList(await response.json());
+      }
+    });
+  }, [debouncedParam]);
+
+  useMount(() => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  });
+
+  return (
+    <div>
+      <SearchPanel param={param} setParam={setParam} users={users} />
+      <List list={list} users={users} />
+    </div>
+  );
+}
